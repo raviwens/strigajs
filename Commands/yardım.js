5d0f2c6d14a6feb2f0e5330bfc0e434105172fa2
raviwens <75216281+raviwens@users.noreply.github.com>
2021-01-26 22:43:10 +0300

    Add files via upload

---
+++ Commands/yardım.js
@@ -0,0 +1,32 @@
+const { dc, MessageEmbed } = require('discord.js')
+const Settings = require('../Settings/Settings.json')
+const Other = require('../Settings/Other.json')
+var prefix = Settings.BotSettings.prefix
+exports.run = async (client, message, args) => {
+  
+    let embed = new MessageEmbed()
+    .setAuthor(message.author.tag, message.author.avatarURL({ dynamic: true }))
+    .setTitle("Register Komutları")
+    .setDescription(`• \`${prefix}erkek/kadın @Üye İsim Yaş\` Üye Kaydı Yapar.
+• \`${prefix}isim @Üye İsim Yaş\` Üye'nin İsmini Değiştirir.
+• \`${prefix}isimler @Üye\` Eski İsimleri Gösterir.
+• \`${prefix}topteyit\` Toplam Kayıt Listesini Gösterir.
+• \`${prefix}stat @Üye\` Kayıt Bilgisini Gösterir.
+• \`${prefix}teyitbilgi @Üye\` İlk Ve Son Kayıtları Gösterir.
+• \`${prefix}kayıtsız @Üye\` Kullanıcıyı Kayıtsıza Atar.`)
+    .setTimestamp()
+    .setFooter(`discord.gg/striga`)
+    .setColor(Settings.Colors.Magenta)
+
+  message.channel.send(embed)
+}
+
+module.exports.conf = {
+  enabled: true,
+  guildOnly: true,
+  aliases: ["yardım"]
+};
+
+module.exports.help = {
+  name: 'yardım'
+};
---
+++ Readme.md
@@ -0,0 +1,15 @@
+🔥 **`LOSINT X RAVİWEN X STRİGA TARAFINDAN YAPILMIŞTIR`** 🔥
+
+✨ **` LOSİNT YOUTUBE KANALIMIN ADRESI`** ✨
+`https://www.youtube.com/channel/UCmAj4AeRNb1kR2O1v9Q9RaA`
+
+✨ **` STRİGA YOUTUBE KANALIMIN ADRESI`** ✨
+ `https://www.youtube.com/channel/UC2yLMwMa0vyrBw_Tzhe0U-g`
+
+🎋 **`DISCROD KOD SUNUCUMUZUN ADRESI`** 🎋
+        `discord.gg/striga`
+
+`İzinli Veyahut İzinsiz Paylaşmassanız Sevinirim`
+
+🎉 **`ALTYAPI HAKKINDA`** 🎉
+`Altyapı Tamamen Kendi Tarafımca Kodlanmıştır Sadece Ufak Yerleri Düzelterek Bu Botu HATASIZ Bir Şekilde Kullanabilirsiniz.`
